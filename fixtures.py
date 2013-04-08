5e046898ac865735e23bddb0a9fcaed3e1e479b0
kagutsuchi07 <kagutsuchipl@gmail.com>
2013-04-08 15:46:11 +0300

    Update fixtures.py

--- fixtures.py
+++ fixtures.py
@@ -8,19 +8,19 @@ pattern = re.findall('<div class="data">(.+)</div><div class="godzina">(.+)</div
 
 # VALUES(Fixture, Round, First_Team, FT_Score, Second_Team, ST_Score, Match_Date)
 
-nr_round = 0
-nr_match = 1
-for value in pattern:
-    score = value[3]
+def insert_values():
     
-    FT = value[2]
-    FTS = score[0]
-    ST = value[4]
-    STS = score[2]
-    
-    if (nr_match - 1) % 10 == 0:
-        nr_round += 1
-        
-    db.query("INSERT INTO PremierLeague VALUES(nr_match, nr_round, FT, FTS, ST, STS, NULL)")
-    nr_match += 1    
-db.close()
+    nr_round = 0
+    nr_match = 1
+
+    for value in pattern_create:
+        FT = value[2]
+        ST = value[3]
+
+        if (nr_match - 1) % 10 == 0:
+            nr_round += 1
+
+        db.execute("INSERT INTO PremierLeague VALUES(%s, %s, %s, NULL, %s, NULL, NULL)", nr_match, nr_round, FT, ST)
+
+        nr_match +=1
+    db.close()
