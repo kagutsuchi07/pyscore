3492a4cc4300eeed16714fa48932cd6557249a65
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2013-04-08 17:30:20 +0200

    update pyscore.py to work with fixtures

--- pyscore.py
+++ pyscore.py
@@ -4,6 +4,7 @@
 """pyscore
 
 Usage:
+    pyscore insert
     pyscore update
     pyscore -h | --help | --version
 
@@ -18,12 +19,20 @@ __version__ = '0.0.1'
 
 from docopt import docopt
 
+from fixtures import insert_values, update_values
+
+
+def insert():
+    insert_values()
+
 
 def update():
-    return 'TEST'
+    update_values()
 
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version=__version__)
     if arguments['update']:
-        print update()
+        update()
+    elif arguments['insert']:
+        insert()
