4eeef1ac7486985045903fb0a48f18ce01d5db27
kagutsuchi07 <kagutsuchipl@gmail.com>
2013-04-10 22:02:53 +0300

    Create func.py

---
+++ func.py
@@ -0,0 +1,76 @@
+import torndb
+
+def new_tournament(tournament_name):
+    """
+    Creates new table where will be kept players and their points
+    WORK IN PROGRESS
+    """
+
+    db = torndb.Connection('db4free.net', 'pyscore', 'login', 'pwd')
+
+    create_tournament = db.execute("IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'pyscore' AND  TABLE_NAME = '%s')) \
+                                    BEGIN CREATE TABLE %s (ID TINYINT(2) AUTO_INCREMENT PRIMARY KEY, Player VARCHAR(20), Points SMALLINT(3) UNSIGNED) END", tournament_name)
+    db.close()
+    print "Created new tournment ", tournment_name
+
+
+def new_player(player_name):
+    """
+    Creates new table where will be kept scores
+    WORK IN PROGRESS
+    """
+
+    db = torndb.Connection('db4free.net', 'pyscore', 'login', 'pwd')
+
+    create_player = db.execute("IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'pyscore' AND  TABLE_NAME = '%s')) \
+                                BEGIN CREATE TABLE %s (ID SMALLINT(4) AUTO_INCREMENT PRIMARY KEY, League VARCHAR(20) INDEX, Home_Team VARCHAR(20) INDEX, \
+                                HT_Score TINYINT(2) UNSIGNED, Away_Team VARCHAR(20) INDEX, AT_Score TINYINT(2) UNSIGNED, Match_Date DATETIME) END", player_name)
+    db.close()
+    print "Created new player ", player_name
+
+    
+def check_score(player_name, league_name = PreamierLeague):
+    """Check if all user typed scores are equal to actual scores and returns user points"""
+
+    db = torndb.Connection('db4free.net', 'pyscore', 'login', 'pwd')
+
+    match = db.query("SELECT First_Team, Second_Team FROM player_name")
+
+    player_points = 0
+
+    for match in match_id:
+
+        home = match['First_Team']
+        away = match['Second_Team']
+
+        player_score = db.get("SELECT FT_Score, ST_Score FROM player_name WHERE League =%s First_Team =%s AND Second_Team = %s", league_name home, away)
+        actual_score = db.get("SELECT FT_Score, ST_Score FROM league_name WHERE First_Team =%s AND Second_Team = %s", home, away)
+
+        p_fts = player_score['FT_Score']
+        p_sts = player_score['ST_Score']
+
+        fts = actual_score['FT_Score']
+        sts = actual_score['ST_Score']
+
+        if (p_fts == fts and p_sts == sts):
+            player_points += 3
+            print home, away, 'Awesome! You got score right! 3 points'
+        elif(p_fts > p_sts and fts > sts):
+            player_points += 1
+            print home, away, 'You got the winner right. 1 point'
+        elif(p_fts < p_sts and fts < sts):
+            player_points += 1
+            print home, away, 'You got the winner right. 1 point'
+    db.close()    
+    return player_points
+
+
+def update_points(player_name, tournament_name):
+    """Updates player points in tournament table"""
+    
+    check_score(player_name)
+    db = torndb.Connection('db4free.net', 'pyscore', 'login', 'pwd')
+
+    db.execute("UPDATE %s SET Points = %s WHERE Player = %s", tournment_name, player_points, player_name)
+    db.close()
+    print "Updated players ", player_name, "points: ", player_points
