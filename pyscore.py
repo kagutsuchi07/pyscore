5ace89d15b40533c44779eb07c00a8ee52fc5ee6
kagutsuchi07 <kagutsuchipl@gmail.com>
2013-04-11 15:19:34 +0300

    update adn instert now operate on various leagues

--- pyscore.py
+++ pyscore.py
@@ -23,13 +23,50 @@ from fixtures import getFixtures, getResults, dbInsertFixtures, dbUpdateResults
 
 
 def insert():
-    fixtures = getFixtures()
-    dbInsertFixtures(fixtures)
+    print "1) Premier League \n 2) Premiera Division \n 3) Ekstraklasa \n \n 0) Exit \n \n Select League or Exit:"
+    while TRUE:
+        select_league = raw_input()
+        if selct_league in (0,1,2,3):
+            break
+        else:
+            print "Unknown Command. Try again"
+
+    if select_league == 1:
+        league = 'PremierLeague'
+        fpr = 10 # fixture per round
+    if select_league == 2:
+        league = 'PremieraDivision'
+        fpr = 10
+    if select_league == 3:
+        league ='Ekstraklasa'
+        fpr = 8
+    if select_league == 0:
+        break
+ 
+    fixtures = getFixtures(league, fpr)
+    dbInsertFixtures(league, fixtures)
 
 
 def update():
-    results = getResults()
-    dbUpdateResults(results)
+    print "1) Premier League \n 2) Premiera Division \n 3) Ekstraklasa \n \n 0) Exit \n \n Select League or Exit:"
+    while TRUE:
+        select_league = raw_input()
+        if selct_league in (0,1,2,3):
+            break
+        else:
+            print "Unknown Command. Try again"
+
+    if select_league == 1:
+        league = 'PremierLeague'
+    if select_league == 2:
+        league = 'PremieraDivision'
+    if select_league == 3:
+        league ='Ekstraklasa'
+    if select_league == 0:
+        break
+ 
+    results = getResults(league)
+    dbUpdateResults(league, results)
 
 
 if __name__ == '__main__':
