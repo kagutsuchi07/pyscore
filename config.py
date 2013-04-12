7826b6d99915b4ef9b38a7923d39b910a4143c47
kagutsuchi07 <kagutsuchipl@gmail.com>
2013-04-12 23:18:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,3 @@ db_host = 'db4free.net'
 db_database = 'pyscore'
 db_user = ''
 db_pass = ''
-
-leagues = {  # id: 'LeagueName',
-    1: 'PremierLeague',
-    2: 'PremieraDivision',
-    3: 'Ekstraklasa',
-}
